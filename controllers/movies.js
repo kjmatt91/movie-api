e59101a9b73349b9688111c5a239add49a4a2527
kjmatt91 <kjmatt91@gmail.com>
2021-08-12 23:19:16 -0400

    set up main functions and controller file

--- controllers/movies.js
+++ controllers/movies.js
@@ -4,4 +4,20 @@ const getAllMovies = (request, response) => {
   return response.send(movies)
 }
 
-module.exports = { getAllMovies }
+const getMovieByDirector = (request, response) => {
+  const { movieName } = request.params
+
+  const foundMovie = movies.filter((movie) => movie.directors === movieName)
+
+  return response.send(foundMovie)
+}
+
+const getMovieByTitle = (request, response) => {
+  const { titleName } = request.params
+
+  const foundTitle = movies.filter((movie) => movie.title === titleName)
+
+  return response.send(foundTitle)
+}
+
+module.exports = { getAllMovies, getMovieByDirector, getMovieByTitle }
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('espress')
-const { getAllMovies } = require('./controllers/movies')
+const express = require('express')
+const { getAllMovies, getMovieByDirector, getMovieByTitle } = require('./controllers/movies')
 
 const app = express()
-const port = 7500
+const port = 8000
 
 app.get('/', getAllMovies)
+
+app.get('/:movieName', getMovieByDirector)
+
+app.get('/:titleName', getMovieByTitle)
 
 app.listen(port, () => {
   console.log('Listening on port ' + port) // eslint-disable-line no-console
